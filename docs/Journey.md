5c55e874e440ebb766b679af4147842b82e178e1
Paul Mundt <paul.mundt@adaptant.io>
2019-05-12 21:36:41 +0200

    KNOWGOCAR-85: Update for Journey data model changes

--- docs/Journey.md
+++ docs/Journey.md
@@ -11,8 +11,13 @@ Name | Type | Description | Notes
 **journeyID** | **String** |  | [default to null]
 **driverID** | **int** |  | [optional] [default to null]
 **autoID** | **int** |  | [optional] [default to null]
+**name** | **String** |  | [optional] [default to null]
+**journeyType** | **List&lt;String&gt;** |  | [optional] [default to []]
 **journeyBegin** | [**DateTime**](DateTime.md) |  | [optional] [default to null]
 **journeyEnd** | [**DateTime**](DateTime.md) |  | [optional] [default to null]
+**odometerBegin** | **double** |  | [optional] [default to null]
+**odometerEnd** | **double** |  | [optional] [default to null]
+**events** | [**List&lt;Event&gt;**](Event.md) |  | [optional] [default to []]
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- lib/model/journey.dart
+++ lib/model/journey.dart
@@ -8,14 +8,24 @@ class Journey {
   
   int autoID = null;
   
+  String name = null;
+  
+  List<String> journeyType = [];
+  
   DateTime journeyBegin = null;
   
   DateTime journeyEnd = null;
+  
+  double odometerBegin = null;
+  
+  double odometerEnd = null;
+  
+  List<Event> events = [];
   Journey();
 
   @override
   String toString() {
-    return 'Journey[journeyID=$journeyID, driverID=$driverID, autoID=$autoID, journeyBegin=$journeyBegin, journeyEnd=$journeyEnd, ]';
+    return 'Journey[journeyID=$journeyID, driverID=$driverID, autoID=$autoID, name=$name, journeyType=$journeyType, journeyBegin=$journeyBegin, journeyEnd=$journeyEnd, odometerBegin=$odometerBegin, odometerEnd=$odometerEnd, events=$events, ]';
   }
 
   Journey.fromJson(Map<String, dynamic> json) {
@@ -35,6 +45,16 @@ class Journey {
     } else {
           autoID = json['AutoID'];
     }
+    if (json['Name'] == null) {
+      name = null;
+    } else {
+          name = json['Name'];
+    }
+    if (json['JourneyType'] == null) {
+      journeyType = null;
+    } else {
+      journeyType = (json['JourneyType'] as List).cast<String>();
+    }
     if (json['JourneyBegin'] == null) {
       journeyBegin = null;
     } else {
@@ -45,6 +65,21 @@ class Journey {
     } else {
       journeyEnd = DateTime.parse(json['JourneyEnd']);
     }
+    if (json['OdometerBegin'] == null) {
+      odometerBegin = null;
+    } else {
+          odometerBegin = json['OdometerBegin'];
+    }
+    if (json['OdometerEnd'] == null) {
+      odometerEnd = null;
+    } else {
+          odometerEnd = json['OdometerEnd'];
+    }
+    if (json['Events'] == null) {
+      events = null;
+    } else {
+      events = Event.listFromJson(json['Events']);
+    }
   }
 
   Map<String, dynamic> toJson() {
@@ -52,8 +87,13 @@ class Journey {
       'JourneyID': journeyID,
       'DriverID': driverID,
       'AutoID': autoID,
+      'Name': name,
+      'JourneyType': journeyType,
       'JourneyBegin': journeyBegin == null ? '' : journeyBegin.toUtc().toIso8601String(),
-      'JourneyEnd': journeyEnd == null ? '' : journeyEnd.toUtc().toIso8601String()
+      'JourneyEnd': journeyEnd == null ? '' : journeyEnd.toUtc().toIso8601String(),
+      'OdometerBegin': odometerBegin,
+      'OdometerEnd': odometerEnd,
+      'Events': events
     };
   }
 
